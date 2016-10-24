41a21a8ca74cebdf5d615eed9f3a95459c5006b5
조우진 <woojin@tmon.co.kr>
2016-10-24 23:42:37 +0900

    또 다른 변경 내용

--- ch_03/cmp-data-view/src/app/app.component.ts
+++ ch_03/cmp-data-view/src/app/app.component.ts
@@ -4,26 +4,20 @@ import { Component } from '@angular/core';
     selector: 'app',
     template: `
         <p>{{contentData}}</p>
+        <p>{{getSomeText(‘Angular2’)}}</p>
+        <p>{{ ‘<‘ + contentData + ‘>’ }}</p>
         <p [textContent]="contentData"></p>
-        <button color="primary" (click)="changeText()">{{getSomeButtonText('abc')}}</button>
     `
 })
 export class AppComponent {
     contentData: string;
-    backupData: string = "데이터 변경의 반영도 Angular가";
 
     constructor() {
         this.contentData = "데이터 노출은 Angular에게 위임을";
     }
 
     // 권장하지 않는 방식, 파이프를 사용하는 것이 더 좋음
-    getSomeButtonText(code: string) {
-        return `입력: ${code}`;
-    }
-
-    changeText() {
-        const tempData = this.contentData;
-        this.contentData = this.backupData;
-        this.backupData = tempData;
+    getSomeText(appName: string) {
+       return `${appName}를 공부하는 방법!`;
     }
 }
